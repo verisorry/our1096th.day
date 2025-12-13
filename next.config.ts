aba466723e9bb61b85f91ec89eb8328fc04db5c9
Silvia Fang <fang.silvia@gmail.com>
2025-12-14 00:47:17 +0800

    feat(grid): basic grid

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
--- src/components/MessageCalendar.tsx
+++ src/components/MessageCalendar.tsx
@@ -31,19 +31,18 @@ export default function MessageCalendar({ data }: MessageGridProps) {
     return (
         <div className="relative w-full overflow-x-auto pb-4">
             {/* desktop */}
-            <div className="hidden md:grid grid-cols-52 gap-1 w-fit mx-auto">
-                {data.map((day) => {
+            <div className="grid gap-1 w-fit mx-auto" style={{ gridTemplateColumns: 'repeat(14, minmax(0, 1fr))' }}>
+                {data.map((day, index) => {
                     return (
-                        <div 
-                            key={day.id}
-                            className="w-3 h-3 rounded cursor-pointer transition-transform hover:scale-110 hover:z-10 bg-gray-500"
+                        <div
+                            key={day.date}
+                            className="w-5 h-5 rounded cursor-pointer transition-transform hover:scale-110 hover:z-10 bg-gray-500"
                             >
 
                         </div>
                     )
                 })}
             </div>
-
         </div>
     )
 
