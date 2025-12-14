dddaca179e7109b12a549a1df3f0abf9b2d4d668
Silvia Fang <fang.silvia@gmail.com>
2025-12-14 16:46:56 +0800

    feat(grid): added colours based on sentiment and frequency, added flowers for milestone

--- src/components/MessageCalendar.tsx
+++ src/components/MessageCalendar.tsx
@@ -1,8 +1,9 @@
 // It takes your data and creates a grid of colored squares
 // Each square represents one day
 
+import { useMemo } from "react";
+
 interface DayData {
-//   id: number;
   date: string;
   messageCount: number;
   fromMe: number;
@@ -15,23 +16,86 @@ interface DayData {
   topEmoji: string;
   era: string;
   milestone: string | null;
-//   isApart: boolean;
 }
 
 interface MessageGridProps {
   data: DayData[];
 }
 
+// helper for colour logic
+const getCellStyle = (day: DayData, maxMessages: number) => {
+    // if no message
+    if (day.messageCount === 0) {
+        return {
+            backgroundColor: 'rgba(31, 41, 55, 0.3)',
+        };
+    }
+
+    const compound = day.sentiment.compound;
+    let rgb;
+
+    // if very negative
+    if (compound < -0.3) rgb = '81, 38, 1';
+
+    // if negative
+    else if (compound < -0.05) rgb = '145, 70, 3';
+
+    // if neutral
+    else if (compound <= 0.05) rgb = '15, 94, 156';
+
+    // if positive
+    else if (compound <= 0.3) rgb = '56, 106, 0';
+
+    // if very positive
+    else rgb = '99, 180, 0';
+
+    // opacity based on message count
+    const opacity = 0.4 + (day.messageCount / maxMessages) * 0.6;
+
+    return { backgroundColor: `rgba(${rgb}, ${opacity})` };
+}
+
+const getFlower = (index: number) => {
+    // const flowers = ['❋','❀', '✿', '❁', '❃', '᪥', '𖠇', '❊', '✺', '🪷', '🌸', '🌷', '🌻', '🌼', '🪻', '🍂']
+    const flowers = ['🪷', '🌸', '🌷', '🌻', '🌼', '🪻', '🍂']
+
+    return flowers[index % flowers.length]
+}
+
 export default function MessageCalendar({ data }: MessageGridProps) {
+    const maxMessages = useMemo(() => 
+        Math.max(...data.map(d => d.messageCount)),
+        [data]
+    );
+
     return (
-        <div className="relative w-full overflow-x-auto pb-4">
-            <div className="grid gap-1 w-fit mx-auto" style={{ gridTemplateColumns: 'repeat(14, minmax(0, 1fr))' }}>
+        <div className="relative w-full pb-4">
+            <div className="grid w-fit mx-auto" style={{ gridTemplateColumns: 'repeat(14, minmax(0, 1fr))' }}>
                 {data.map((day, index) => {
+                    const style = getCellStyle(day, maxMessages);
+
                     return (
                         <div
                             key={day.date}
-                            className="w-5 h-5 rounded cursor-pointer transition-transform hover:scale-110 hover:z-10 bg-gray-500"
+                            className={`
+                                w-8 h-8
+                                cursor-pointer 
+                                transition-transform 
+                                hover:scale-110
+                                hover:z-10
+                                text-center
+                                align-middle
+                                leading-8
+                                `}
+                            style={style}
                             >
+
+                                {/* grow a flower if a milestone */}
+                                {
+                                (day.milestone)
+                                    ? <span className="text-2xl"> {getFlower(index)} </span>
+                                    : null
+                                }
 
                         </div>
                     )
@@ -39,7 +103,4 @@ export default function MessageCalendar({ data }: MessageGridProps) {
             </div>
         </div>
     )
-
-
-
 }
